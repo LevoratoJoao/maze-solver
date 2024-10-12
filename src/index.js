29c48bb2555a862e4b05ff119bb5ee63e73f1b32
LevoratoJoao <j.vitor.jvlds@gmail.com>
2024-10-12 13:31:07 -0300

    Start and Goal on board

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 "use strict";
 const BOARD_ROWS = 16; // TODO: Add input to decide how many rows and cols the board has
 const BOARD_COLS = BOARD_ROWS;
-const stateColor = ["#202020", "#FF5050", "#50FF50", "#50FFFF"]; // 0: path, 1: wall, 2: start, 3: goal
-function createBoard() {
-    const board = [];
-    for (let r = 0; r < BOARD_ROWS; r++) {
-        board.push(new Array(BOARD_COLS).fill(0));
-    }
-    return board;
-}
+// 0: path, 1: wall, 2: start, 3: goal, 4: route
+const stateColor = ["#202020", "#555555", "#50FF50", "#50FFFF", "#FF5050"];
 class Game {
     constructor() {
         this._board = [];
         this._start = [];
         this._goal = [];
-        this._board = createBoard();
+        this._board = this.createBoard();
         this._canvas = document.getElementById("app");
         if (this._canvas === null) {
             throw new Error('Could not find canvas');
@@ -38,11 +32,24 @@ class Game {
         }
         return this._ctx;
     }
-    render() {
+    get start() {
+        return this._start;
+    }
+    get goal() {
+        return this._goal;
+    }
+    createBoard() {
+        const board = [];
         for (let r = 0; r < BOARD_ROWS; r++) {
-            for (let c = 0; c < BOARD_COLS; c++) {
-                const x = r * CELL_WIDTH;
-                const y = c * CELL_HEIGHT;
+            board.push(new Array(BOARD_COLS).fill(0));
+        }
+        return board;
+    }
+    render() {
+        for (let r = 0; r < BOARD_ROWS; ++r) {
+            for (let c = 0; c < BOARD_COLS; ++c) {
+                const x = r * CELL_HEIGHT;
+                const y = c * CELL_WIDTH;
                 if (this._ctx) {
                     switch (this._board[r][c]) {
                         case 0:
@@ -54,25 +61,14 @@ class Game {
                             this._ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
                             break;
                         case 2:
-                            if (this._start.length > 0) {
-                                console.log("You clicked on anotha start");
-                                this._ctx.fillStyle = stateColor[0];
-                                this._ctx.fillRect(this._goal[0], this._goal[1], CELL_WIDTH, CELL_HEIGHT);
-                            }
                             this._start = [r, c];
-                            console.log(this._start);
                             this._ctx.fillStyle = stateColor[2];
                             this._ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
                             break;
                         case 3:
-                            if (this._goal.length > 0) {
-                                this._ctx.fillStyle = stateColor[0];
-                                this._ctx.fillRect(this._goal[0], this._goal[1], CELL_WIDTH, CELL_HEIGHT);
-                            }
+                            this._goal = [r, c];
                             this._ctx.fillStyle = stateColor[3];
                             this._ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
-                            console.log(this._goal);
-                            this._goal = [r, c];
                             break;
                         default:
                             break;
@@ -92,6 +88,12 @@ game.canvas.addEventListener('click', (e) => {
     const state = document.getElementsByName("state");
     for (let i = 0; i < state.length; i++) {
         if (state[i].checked) {
+            if (i === 2 && game.start.length > 0) {
+                game.board[game.start[0]][game.start[1]] = 0;
+            }
+            if (i === 3 && game.goal.length > 0) {
+                game.board[game.goal[0]][game.goal[1]] = 0;
+            }
             game.board[col][row] = i;
             game.render();
             return;
--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,8 @@ const BOARD_COLS = BOARD_ROWS;
 // Types and colors
 type State = number;
 type Board = State[][];
-const stateColor = ["#202020", "#FF5050", "#50FF50", "#50FFFF"] // 0: path, 1: wall, 2: start, 3: goal
-
-function createBoard(): Board {
-    const board: Board = [];
-    for (let r = 0; r < BOARD_ROWS; r++) {
-        board.push(new Array<State>(BOARD_COLS).fill(0));
-    }
-    return board;
-}
+// 0: path, 1: wall, 2: start, 3: goal, 4: route
+const stateColor = ["#202020", "#555555", "#50FF50", "#50FFFF", "#FF5050"]
 
 class Game {
     private _canvas: HTMLCanvasElement;
@@ -22,7 +15,7 @@ class Game {
     private _goal: number[] = [];
 
     constructor() {
-        this._board = createBoard();
+        this._board = this.createBoard();
         this._canvas = document.getElementById("app") as HTMLCanvasElement;
         if (this._canvas === null) {
             throw new Error('Could not find canvas');
@@ -50,11 +43,27 @@ class Game {
         return this._ctx;
     }
 
-    public render() {
+    public get start(): number[] {
+        return this._start;
+    }
+
+    public get goal(): number[] {
+        return this._goal;
+    }
+
+    private createBoard(): Board {
+        const board: Board = [];
         for (let r = 0; r < BOARD_ROWS; r++) {
-            for (let c = 0; c < BOARD_COLS; c++) {
-                const x = r * CELL_WIDTH;
-                const y = c * CELL_HEIGHT;
+            board.push(new Array<State>(BOARD_COLS).fill(0));
+        }
+        return board;
+    }
+
+    public render() {
+        for (let r = 0; r < BOARD_ROWS; ++r) {
+            for (let c = 0; c < BOARD_COLS; ++c) {
+                const x = r * CELL_HEIGHT;
+                const y = c * CELL_WIDTH;
                 if (this._ctx) {
                     switch (this._board[r][c]) {
                         case 0:
@@ -66,26 +75,14 @@ class Game {
                             this._ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
                             break;
                         case 2:
-                            if (this._start.length > 0) {
-                                console.log("You clicked on anotha start");
-                                this._ctx.fillStyle = stateColor[0];
-                                this._ctx.fillRect(this._goal[0], this._goal[1], CELL_WIDTH, CELL_HEIGHT);
-
-                            }
                             this._start = [r, c];
-                            console.log(this._start);
                             this._ctx.fillStyle = stateColor[2];
                             this._ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
                             break;
                         case 3:
-                            if (this._goal.length > 0) {
-                                this._ctx.fillStyle = stateColor[0];
-                                this._ctx.fillRect(this._goal[0], this._goal[1], CELL_WIDTH, CELL_HEIGHT);
-                            }
+                            this._goal = [r, c];
                             this._ctx.fillStyle = stateColor[3];
                             this._ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
-                            console.log(this._goal);
-                            this._goal = [r, c];
                             break;
 
                         default:
@@ -111,6 +108,12 @@ game.canvas.addEventListener('click', (e) => {
     const state = document.getElementsByName("state");
     for (let i = 0; i < state.length; i++) {
         if ((state[i] as HTMLInputElement).checked) {
+            if (i === 2 && game.start.length > 0) {
+                game.board[game.start[0]][game.start[1]] = 0;
+            }
+            if (i === 3 && game.goal.length > 0) {
+                game.board[game.goal[0]][game.goal[1]] = 0;
+            }
             game.board[col][row] = i;
             game.render();
             return;
