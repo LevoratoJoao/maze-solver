d79b22eb25bb19fd30aca174a5082491ab848418
LevoratoJoao <j.vitor.jvlds@gmail.com>
2024-12-23 01:38:35 -0300

    Cell interface and hill climbing or best choice, dont remember

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,37 @@ const BOARD_COLS = BOARD_ROWS;
 type Board = number[][];
 // 0: path, 1: wall, 2: start, 3: goal, 4: route
 const stateColor = ["#202020", "#555555", "#50FF50", "#50FFFF", "#FF5050"];
+let pathTimer: NodeJS.Timeout | null = null;
+
+interface Cell {
+    position: number[];
+    stringValue: string;
+    cost: number;
+}
 
 class Game {
+
     private _canvas: HTMLCanvasElement;
     private _ctx: CanvasRenderingContext2D | null;
     private _board: Board = [];
     private _start: number[] = [];
     private _goal: number[] = [];
+    private _CELL_WIDTH: number = 0;
+    private _CELL_HEIGHT: number = 0;
 
     constructor() {
-        this._board = this.createBoard();
+        this._board = [];
+        this.createEmptyBoard();
+        this.createRandomBoard = this.createRandomBoard.bind(this);
+        this.createEmptyBoard = this.createEmptyBoard.bind(this);
         this._canvas = document.getElementById("app") as HTMLCanvasElement;
         if (this._canvas === null) {
             throw new Error('Could not find canvas');
         }
         this._canvas.width = 400;
         this._canvas.height = 400;
+        this.CELL_HEIGHT = this._canvas.height / BOARD_ROWS;
+        this.CELL_WIDTH = this._canvas.height / BOARD_COLS;
         this._ctx = this._canvas.getContext("2d");
         if (this._ctx === null) {
             throw new Error('Could not initialize 2d context');
@@ -50,42 +65,85 @@ class Game {
         return this._start;
     }
 
+    public set start(s: number[]) {
+        this._start = s;
+    }
+
     public get goal(): number[] {
         return this._goal;
     }
 
-    public createBoard(): Board {
+    public get CELL_WIDTH(): number {
+        return this._CELL_WIDTH;
+    }
+
+    public set CELL_WIDTH(value: number) {
+        this._CELL_WIDTH = value;
+    }
+
+    public get CELL_HEIGHT(): number {
+        return this._CELL_HEIGHT;
+    }
+
+    public set CELL_HEIGHT(value: number) {
+        this._CELL_HEIGHT = value;
+    }
+
+    public createEmptyBoard() {
         const board: Board = [];
         for (let r = 0; r < BOARD_ROWS; r++) {
             board.push(new Array<number>(BOARD_COLS).fill(0));
         }
-        return board;
+        this._board = board;
+        this._goal = [];
+        this._start = [];
+        this.render();
+    }
+
+    public createRandomBoard() {
+        const board: Board = [];
+        for (let r = 0; r < BOARD_ROWS; r++) {
+            board[r] = []
+            for (let c = 0; c < BOARD_COLS; c++) {
+                const x = r * this.CELL_HEIGHT;
+                const y = c * this.CELL_WIDTH;
+                let rndColor = Math.floor(Math.random() * 2)
+                board[r][c] = rndColor;
+            }
+        }
+        this._board = board;
+        this._goal = [];
+        this._start = [];
+        this.render();
     }
 
     public render() {
+        if (pathTimer) {
+            clearTimeout(pathTimer);
+        }
         for (let r = 0; r < BOARD_ROWS; ++r) {
             for (let c = 0; c < BOARD_COLS; ++c) {
-                const x = r * CELL_HEIGHT;
-                const y = c * CELL_WIDTH;
+                const x = r * this.CELL_HEIGHT;
+                const y = c * this.CELL_WIDTH;
                 if (this._ctx) {
                     switch (this._board[r][c]) {
                         case 0:
                             this._ctx.fillStyle = stateColor[0];
-                            this._ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
+                            this._ctx.fillRect(x, y, this.CELL_WIDTH, this.CELL_HEIGHT);
                             break;
                         case 1:
                             this._ctx.fillStyle = stateColor[1];
-                            this._ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
+                            this._ctx.fillRect(x, y, this.CELL_WIDTH, this.CELL_HEIGHT);
                             break;
                         case 2:
                             this._start = [r, c];
                             this._ctx.fillStyle = stateColor[2];
-                            this._ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
+                            this._ctx.fillRect(x, y, this.CELL_WIDTH, this.CELL_HEIGHT);
                             break;
                         case 3:
                             this._goal = [r, c];
                             this._ctx.fillStyle = stateColor[3];
-                            this._ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
+                            this._ctx.fillRect(x, y, this.CELL_WIDTH, this.CELL_HEIGHT);
                             break;
 
                         default:
@@ -100,13 +158,9 @@ class Game {
 
 const game = new Game();
 
-// Creating the cells
-const CELL_WIDTH = game.canvas.width / BOARD_COLS;
-const CELL_HEIGHT = game.canvas.height / BOARD_ROWS;
-
 game.canvas.addEventListener('click', (e) => {
-    const col = Math.floor(e.offsetX / CELL_WIDTH);
-    const row = Math.floor(e.offsetY / CELL_HEIGHT);
+    const col = Math.floor(e.offsetX / game.CELL_WIDTH);
+    const row = Math.floor(e.offsetY / game.CELL_HEIGHT);
 
     const state = document.getElementsByName("state");
     for (let i = 0; i < state.length; i++) {
@@ -132,8 +186,8 @@ game.canvas.addEventListener('mousedown', () => {
 
 game.canvas.addEventListener("mousemove", (e) => {
     if (isMouseDown) {
-        let col = Math.floor(e.offsetX / CELL_WIDTH);
-        let row = Math.floor(e.offsetY / CELL_HEIGHT);
+        let col = Math.floor(e.offsetX / game.CELL_WIDTH);
+        let row = Math.floor(e.offsetY / game.CELL_HEIGHT);
         const selectedState = document.querySelector('input[name="state"]:checked') as HTMLInputElement;
         if (selectedState && selectedState.value === "wall") {
             game.board[col][row] = 1;
@@ -150,67 +204,75 @@ game.canvas.addEventListener('mouseup', () => {
 
 game.render();
 
-function randomBoard() {
-    const board: Board = game.createBoard();
-    for (let r = 0; r < BOARD_ROWS; ++r) {
-        for (let c = 0; c < BOARD_COLS; ++c) {
-            const x = r * CELL_HEIGHT;
-            const y = c * CELL_WIDTH;
-            let rndColor = Math.floor(Math.random() * 2)
-            board[r][c] = rndColor;
-        }
-    }
-    game.board = board;
-    game.render();
-}
+let mazePath: number[][] = [];
 
-let path: number[][] = [];
+document.getElementById("solve")?.addEventListener("click", solve);
+document.getElementById("randomBoard")?.addEventListener("click", game.createRandomBoard);
+document.getElementById("resetPath")?.addEventListener("click", resetPath);
+document.getElementById("resetBoard")?.addEventListener("click", game.createEmptyBoard);
 
 function solve() {
     const selectElement = document.getElementById('solve-options') as HTMLSelectElement;
-
+    console.log("Game board: ", game.board);
+    console.log("Start: ", game.start);
+    console.log("Goal: ", game.goal);
     switch (selectElement.selectedIndex) {
         case 1:
-            path = dfs();
+            mazePath = dfs();
             break;
         case 2:
-            path = bfs();
+            mazePath = bfs();
             break;
         case 3:
-
+            mazePath = bestChoice();
             break;
         default:
             break;
     }
-    for (const p of path) {
-        const x = p[0] * CELL_HEIGHT;
-        const y = p[1] * CELL_WIDTH;
-
-        game.ctx.fillStyle = stateColor[4];
-        game.ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
+    let index = 0;
+    function fillPath() {
+        if (index < mazePath.length) {
+            if ((mazePath[index][0] != game.goal[0] || mazePath[index][1] != game.goal[1])) {
+                const x = mazePath[index][0] * game.CELL_HEIGHT;
+                const y = mazePath[index][1] * game.CELL_WIDTH;
+                game.ctx.fillStyle = stateColor[4];
+                game.ctx.fillRect(x, y, game.CELL_WIDTH, game.CELL_HEIGHT);
+            }
+            index++;
+            pathTimer = setTimeout(fillPath, 100);
+        }
     }
+    fillPath();
 }
 
 function resetPath() {
-    for (const p of path) {
-        const x = p[0] * CELL_HEIGHT;
-        const y = p[1] * CELL_WIDTH;
+    if (pathTimer) {
+        clearTimeout(pathTimer);
+    }
+    for (let i = 0; i < mazePath.length; i++) {
+        const x = mazePath[i][0] * game.CELL_HEIGHT;
+        const y = mazePath[i][1] * game.CELL_WIDTH;
 
         game.ctx.fillStyle = stateColor[0];
-        game.ctx.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
+        game.ctx.fillRect(x, y, game.CELL_WIDTH, game.CELL_HEIGHT);
     }
+    mazePath = [];
 }
 
-function resetBoard() {
-    game.board = game.createBoard();
-    game.render();
-}
-
+/**
+ *  TODO: More optimized, try latter
+    const directions = [
+        [-1, 0], [1, 0], // vertical
+        [0, -1], [0, 1]  // horizontal
+    ];
+    const neighbours = [];
+    for (const [dr, dc] of directions) {
+ */
 function getNeighbours(cell: number[]): number[][] {
-    let ngb = [];
+    let ngb: number[][] = [];
     for (let dr = -1; dr <= 1; ++dr) {
         for (let dc = -1; dc <= 1; ++dc) {
-            if (dr != 0 || dc != 0) {
+            if ((dr != 0 || dc != 0) && (dr != dc) && (dr != -dc)) {
                 const r = cell[0] + dr;
                 const c = cell[1] + dc;
                 if ((0 <= r && r < BOARD_ROWS) && (0 <= c && c < BOARD_COLS)) {
@@ -225,47 +287,106 @@ function getNeighbours(cell: number[]): number[][] {
 }
 
 function dfs(): number[][] {
-    const visitedCells = new Map<string, number[]>();
+    const visitedCells = new Set<string>();
     const stack: number[][] = [game.start];
-    const path: number[][] = [];
+    const mazePath: number[][] = [];
 
     while (stack.length > 0) {
         const currentCell = stack.pop()!;
+        if (game.goal.toString() === currentCell.toString()) {
+            console.log("Goal was found, path: ", mazePath);
+            mazePath.pop();
+            return mazePath;
+        }
         if (!visitedCells.has(currentCell.toString())) {
-            visitedCells.set(currentCell.toString(), currentCell);
+            visitedCells.add(currentCell.toString());
             // Explore neighboring cells
             for (const neighbour of getNeighbours(currentCell)) {
-                if (game.goal.every((val, index) => val === neighbour[index])) {
-                    return path;
-                }
                 if (!visitedCells.has(neighbour.toString())) {
-                    path.push(neighbour);
+                    mazePath.push(neighbour);
                     stack.push(neighbour);
                 }
             }
         }
     }
-    return path;
+    console.log("Goal was not found, current path: ", mazePath);
+    return mazePath;
 }
 
 function bfs(): number[][] {
-    const visitedCells = new Map<string, number[]>();
+    const visitedCells = new Set<string>();
     const queue: number[][] = [game.start];
-    const path: number[][] = [];
-    visitedCells.set(game.start.toString(), game.start);
+    const mazePath: number[][] = [];
+
     while (queue.length > 0) {
-        const currentCell = queue.pop()!;
-        // Explore neighboring cells
-        for (const neighbour of getNeighbours(currentCell)) {
-            if (game.goal.every((val, index) => val === neighbour[index])) {
-                return path;
-            }
-            if (!visitedCells.has(neighbour.toString())) {
-                visitedCells.set(currentCell.toString(), currentCell);
-                path.push(neighbour);
-                queue.unshift(neighbour);
+        const currentCell = queue.shift()!;
+        if (game.goal.toString() === currentCell.toString()) {
+            console.log("Goal was found, path: ", mazePath);
+            mazePath.pop();
+            return mazePath;
+        }
+        if (!visitedCells.has(currentCell.toString())) {
+            visitedCells.add(currentCell.toString());
+            // Explore neighboring cells
+            for (const neighbour of getNeighbours(currentCell)) {
+                if (!visitedCells.has(neighbour.toString())) {
+                    mazePath.push(neighbour);
+                    queue.push(neighbour);
+                }
             }
         }
     }
-    return path;
+    console.log("Goal was not found, current path: ", mazePath);
+    return mazePath;
+}
+
+function manhanttanHeuristic(neighbour: number[], goal: number[]): number {
+    let xy1 = neighbour;
+    let xy2 = goal;
+    return Math.abs(xy1[0] - xy2[0]) + Math.abs(xy1[1] - xy2[1]);
+}
+
+function findBestNeighbour(neighbours: Array<Cell>): Cell {
+    let bestNeighbour: Cell = { position: [], stringValue: "", cost: -1};
+    let bestCost = Infinity;
+    for (const neighbour of neighbours) {
+        const cost = neighbour.cost
+        if (cost < bestCost) {
+            bestNeighbour = neighbour;
+            bestCost = cost;
+        }
+    }
+    return bestNeighbour;
+}
+
+function bestChoice(): number[][] {
+    let currentCell: Cell = {position: game.start, stringValue: game.start.toString(), cost: 0};
+    const visitedCells = new Set<string>();
+    const ngbsHeuristic = new Array<Cell>();
+    const mazePath: number[][] = [];
+
+    ngbsHeuristic.push(currentCell);
+
+    while (ngbsHeuristic.length > 0) {
+        const poppedCell = ngbsHeuristic.pop();
+        if (!poppedCell) {
+            break;
+        }
+        currentCell = poppedCell;
+        if (game.goal.toString() === currentCell.stringValue) {
+            console.log("Goal was found, path: ", mazePath);
+            mazePath.pop();
+            return mazePath;
+        }
+        if (!visitedCells.has(currentCell.stringValue)) {
+            visitedCells.add(currentCell.stringValue);
+            for (const neighbour of getNeighbours(currentCell.position)) {
+                ngbsHeuristic.push({position: neighbour, stringValue: neighbour.toString(), cost: manhanttanHeuristic(neighbour, game.goal)});
+            }
+            currentCell = findBestNeighbour(ngbsHeuristic);
+            mazePath.push(currentCell.position);
+        }
+    }
+    console.log("Goal was not found, current path: ", mazePath);
+    return mazePath;
 }
